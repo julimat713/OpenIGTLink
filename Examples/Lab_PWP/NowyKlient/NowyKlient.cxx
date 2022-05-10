36bdfdf531f52ad132f891b8811a6ccad1b41449
julimat713 <58738112+julimat713@users.noreply.github.com>
2022-05-11 00:47:04 +0200

    Update NowyKlient

--- Examples/Lab_PWP/NowyKlient/NowyKlient.cxx
+++ Examples/Lab_PWP/NowyKlient/NowyKlient.cxx
@@ -18,7 +18,6 @@
 #include "igtlOSUtil.h"
 #include "igtlPointMessage.h"
 #include "igtlClientSocket.h"
-#include "igtlMessageHeader.h"
 
 
 int main(int argc, char* argv[])
@@ -52,8 +51,6 @@ int main(int argc, char* argv[])
     }
 
   //------------------------------------------------------------
-
-
   // Allocate Transform Message Class
 
   igtl::PointMessage::Pointer pointMsg;
@@ -104,61 +101,10 @@ int main(int argc, char* argv[])
   // Send
   socket->Send(pointMsg->GetPackPointer(), pointMsg->GetPackSize());
   
-  igtl::MessageHeader::Pointer headerMsg;
-  headerMsg = igtl::MessageHeader::New()
-  	while(1){
-  		message->ReceivePoint(socket,);
-  		message->Unpack();
-  	}
+  
   //------------------------------------------------------------
   // Close the socket
   socket->CloseSocket();
 
 }
-  #if OpenIGTLink_PROTOCOL_VERSION >= 2
-int ReceivePoint(igtl::Socket * socket)
-{
 
-  std::cerr << "Receiving POINT data type." << std::endl;
-
-  // Create a message buffer to receive transform data
-  igtl::PointMessage::Pointer pointMsg;
-  pointMsg = igtl::PointMessage::New();
- 
-  pointMsg->AllocatePack();
-
-  // Receive transform data from the socket
-  bool timeout(false);
-  socket->Receive(pointMsg->GetPackBodyPointer(), pointMsg->GetPackBodySize(), timeout);
-
-  // Deserialize the transform data
-  // If you want to skip CRC check, call Unpack() without argument.
-  int c = pointMsg->Unpack(1);
-
-  if (c & igtl::MessageHeader::UNPACK_BODY) // if CRC check is OK
-    {
-    int nElements = pointMsg->GetNumberOfPointElement();
-    for (int i = 0; i < nElements; i ++)
-      {
-      igtl::PointElement::Pointer pointElement;
-      pointMsg->GetPointElement(i, pointElement);
-
-      igtlUint8 rgba[4];
-      pointElement->GetRGBA(rgba);
-
-      igtlFloat32 pos[3];
-      pointElement->GetPosition(pos);
-
-      std::cerr << "========== Element #" << i << " ==========" << std::endl;
-      std::cerr << " Name      : " << pointElement->GetName() << std::endl;
-      std::cerr << " GroupName : " << pointElement->GetGroupName() << std::endl;
-      std::cerr << " RGBA      : ( " << (int)rgba[0] << ", " << (int)rgba[1] << ", " << (int)rgba[2] << ", " << (int)rgba[3] << " )" << std::endl;
-      std::cerr << " Position  : ( " << std::fixed << pos[0] << ", " << pos[1] << ", " << pos[2] << " )" << std::endl;
-      std::cerr << " Radius    : " << std::fixed << pointElement->GetRadius() << std::endl;
-      std::cerr << " Owner     : " << pointElement->GetOwner() << std::endl;
-      std::cerr << "================================" << std::endl;
-      }
-    }
-
-  return 1;
-}
